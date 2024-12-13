2a4e91e20401d7fe5295b37d021952676a3b7682
julia <s2310456025@fhooe.at>
2024-12-13 17:31:51 +0100

    eighth commit 13.12. -language edit -edit card button

--- packages/kwm2024/ext_localconf.php
+++ packages/kwm2024/ext_localconf.php
@@ -11,3 +11,6 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['kwm2024'] = 'EXT:kwm2024/Configur
  * PageTS
  */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:kwm2024/Configuration/TsConfig/Page/All.tsconfig">');
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:news/Resources/Private/Language/locallang.xlf'][] = 'EXT:kwm2024/Resources/Private/Language/locallang.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:news/Resources/Private/Language/locallang.xlf'][] = 'EXT:kwm2024/Resources/Private/Language/de.locallang.xlf';
